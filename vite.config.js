259ecc8413675df99fe707a5972995214f14caf7
tanbinali <tanbinali@gmail.com>
2026-09-14 12:58:31 +0600

    Updated ViteConfig

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     imagetools(),
     sitemap({
-      hostname: "https://mohammadkhanautoparts.com",
+      hostname: "https://www.jajicarbatteryautospareparts.com",
       outDir: "./dist",
     }),
   ],
